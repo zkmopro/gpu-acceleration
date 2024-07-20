30ba45be7a75ce6d1fec8eb67656862c0641f5f0
moven0831 <moven0831@gmail.com>
2024-07-20 16:50:23 +0900

    feat: config swift bindings and import MSMBenchmarkView

--- gpu-exploration-app/ios/mopro-test/mopro_testApp.swift
+++ gpu-exploration-app/ios/mopro-test/mopro_testApp.swift
@@ -11,7 +11,8 @@ import SwiftUI
 struct mopro_testApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            // ContentView()
+            MSMBenchmarkView()
         }
     }
 }
