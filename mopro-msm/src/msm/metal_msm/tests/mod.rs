9317493ab792f4e0d41eb09876ef1658145dbe83
moven0831 <moven0831@gmail.com>
2024-11-07 18:51:19 +0700

    test(mont_mul): adapted mont mul tests from https://github.com/geometryxyz/msl-secp256k1

--- mopro-msm/src/msm/metal_msm/tests/mod.rs
+++ mopro-msm/src/msm/metal_msm/tests/mod.rs
@@ -2,3 +2,5 @@
 pub mod bigint;
 #[cfg(test)]
 pub mod field;
+#[cfg(test)]
+pub mod mont_backend;
---
+++ mopro-msm/src/msm/metal_msm/tests/mont_backend/mod.rs
@@ -0,0 +1,4 @@
+#[cfg(test)]
+pub mod mont_mul_modified;
+#[cfg(test)]
+pub mod mont_mul_optimised;
