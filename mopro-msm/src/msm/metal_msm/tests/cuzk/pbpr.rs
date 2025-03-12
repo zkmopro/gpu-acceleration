2b689cfd4268059e5768f1906a3a72178dd453ee
CHUNG, Fu Chuan <2941673c@student.gla.ac.uk>
2025-03-12 15:37:19 +0800

    chore: remove pbpr_bigint

--- mopro-msm/src/msm/metal_msm/tests/cuzk/pbpr.rs
+++ mopro-msm/src/msm/metal_msm/tests/cuzk/pbpr.rs
@@ -1,8 +1,6 @@
 use crate::msm::metal_msm::host::gpu::{create_buffer, get_default_device};
 use crate::msm::metal_msm::host::shader::{compile_metal, write_constants};
-use crate::msm::metal_msm::utils::data_conversion::{
-    points_from_gpu_buffer, points_to_gpu_buffer, raw_reduction,
-};
+use crate::msm::metal_msm::utils::data_conversion::raw_reduction;
 use crate::msm::metal_msm::utils::limbs_conversion::GenericLimbConversion;
 use crate::msm::metal_msm::utils::mont_params::{calc_mont_radix, MontgomeryParams};
 use ark_bn254::{Fq as BaseField, Fr as ScalarField, G1Projective as G};
@@ -33,111 +31,8 @@ fn closest_power_of_two(n: usize) -> usize {
     }
 }
 
-// Implements parallel bucket reduction in GPU
-fn gpu_parallel_bpr(buckets: &Vec<G>) -> G {
-    let mont_params = MontgomeryParams::default();
-    let log_limb_size: u32 = 16;
-    let num_limbs = mont_params.num_limbs;
-    let n0 = mont_params.n0;
-    let nsafe = mont_params.nsafe;
-
-    let device = get_default_device();
-    let bucket_size = buckets.len();
-    let bucket_buffer = points_to_gpu_buffer(buckets, num_limbs, &device);
-
-    let library_path = compile_metal("../mopro-msm/src/msm/metal_msm/shader/cuzk", "pbpr.metal");
-    let library = device.new_library_with_file(library_path).unwrap();
-    let kernel = library.get_function("parallel_bpr", None).unwrap();
-
-    let pipeline_state_descriptor = ComputePipelineDescriptor::new();
-    pipeline_state_descriptor.set_compute_function(Some(&kernel));
-
-    let pipeline_state = device
-        .new_compute_pipeline_state_with_function(
-            pipeline_state_descriptor.compute_function().unwrap(),
-        )
-        .unwrap();
-
-    let thread_group_width = pipeline_state.thread_execution_width();
-    let thread_group_height =
-        pipeline_state.max_total_threads_per_threadgroup() / thread_group_width;
-
-    let max_group_threads = pipeline_state.max_total_threads_per_threadgroup();
-
-    let optimal_threads = max_group_threads;
-
-    let candidate = if bucket_size < optimal_threads as usize {
-        bucket_size
-    } else {
-        optimal_threads as usize // This is wrong, since we're workloading just a single TG,
-                                 // The thing is that this gives the best performance so far
-                                 // But we need to introduce cross kernel synchronisation to improve this.
-    };
-
-    let total_threads = closest_power_of_two(candidate) as usize;
-
-    let grid_width = (total_threads as f64).sqrt().ceil() as u64;
-    let grid_height = (total_threads as u64 + grid_width - 1) / grid_width;
-
-    let threads_per_thread_group = MTLSize {
-        width: thread_group_width,
-        height: thread_group_height,
-        depth: 1,
-    };
-
-    let threads_total = MTLSize {
-        width: grid_width,
-        height: grid_height,
-        depth: 1,
-    };
-
-    let total_threads_buffer = create_buffer(&device, &vec![total_threads as u32]);
-    let grid_width_buffer = create_buffer(&device, &vec![grid_width as u32]);
-
-    let m_shared = vec![G::zero(); total_threads];
-    let s_shared = vec![G::zero(); total_threads];
-
-    let m_shared_buffer = points_to_gpu_buffer(&m_shared, num_limbs, &device);
-    let s_shared_buffer = points_to_gpu_buffer(&s_shared, num_limbs, &device);
-
-    let num_subtask = (bucket_size + total_threads - 1) / total_threads;
-    let num_subtask_buffer = create_buffer(&device, &vec![num_subtask as u32]);
-
-    // set up command queue and encoder
-    let command_queue = device.new_command_queue();
-    let command_buffer = command_queue.new_command_buffer();
-    let encoder =
-        command_buffer.compute_command_encoder_with_descriptor(ComputePassDescriptor::new());
-
-    write_constants(
-        "../mopro-msm/src/msm/metal_msm/shader",
-        num_limbs,
-        log_limb_size,
-        n0,
-        nsafe,
-    );
-
-    encoder.set_compute_pipeline_state(&pipeline_state);
-
-    encoder.set_buffer(0, Some(&bucket_buffer), 0);
-    encoder.set_buffer(1, Some(&m_shared_buffer), 0);
-    encoder.set_buffer(2, Some(&s_shared_buffer), 0);
-    encoder.set_buffer(3, Some(&grid_width_buffer), 0);
-    encoder.set_buffer(4, Some(&total_threads_buffer), 0);
-    encoder.set_buffer(5, Some(&num_subtask_buffer), 0);
-
-    encoder.dispatch_threads(threads_total, threads_per_thread_group);
-    encoder.end_encoding();
-    command_buffer.commit();
-    command_buffer.wait_until_completed();
-
-    let s_shared = points_from_gpu_buffer(&s_shared_buffer, num_limbs);
-
-    s_shared.iter().sum::<G>()
-}
-
 /// Implement parallel bucket reduction in GPU using separated buffers internally
-fn gpu_parallel_bpr_big_int(buckets: &Vec<G>) -> G {
+fn gpu_parallel_bpr(buckets: &Vec<G>) -> G {
     /// Converts a bucket vector into three separated vectors for the x, y, and z coordinates
     /// Each coordinate is represented as a vector of u32 values in Montgomery form
     fn buckets_to_separated_coords(
@@ -223,6 +118,7 @@ fn gpu_parallel_bpr_big_int(buckets: &Vec<G>) -> G {
 
         points
     }
+
     let mont_params = MontgomeryParams::default();
     let log_limb_size: u32 = 16;
     let num_limbs = mont_params.num_limbs;
@@ -243,7 +139,7 @@ fn gpu_parallel_bpr_big_int(buckets: &Vec<G>) -> G {
     // Compile shader and create pipeline
     let library_path = compile_metal("../mopro-msm/src/msm/metal_msm/shader/cuzk", "pbpr.metal");
     let library = device.new_library_with_file(library_path).unwrap();
-    let kernel = library.get_function("parallel_bpr_big_int", None).unwrap();
+    let kernel = library.get_function("parallel_bpr", None).unwrap();
 
     let pipeline_state_descriptor = ComputePipelineDescriptor::new();
     pipeline_state_descriptor.set_compute_function(Some(&kernel));
@@ -265,7 +161,9 @@ fn gpu_parallel_bpr_big_int(buckets: &Vec<G>) -> G {
     let candidate = if bucket_size < optimal_threads as usize {
         bucket_size
     } else {
-        optimal_threads as usize
+        optimal_threads as usize // This is wrong, since we're workloading just a single TG,
+                                 // The thing is that this gives the best performance so far
+                                 // But we need to introduce cross kernel synchronisation to improve this.
     };
 
     let total_threads = closest_power_of_two(candidate);
@@ -517,22 +415,4 @@ fn test_pbpr_random_inputs() {
         );
         assert_eq!(gpu_pbpr_result, naive_result);
     }
-}
-
-#[test]
-#[serial_test::serial]
-fn test_pbpr_big_int() {
-    let generator = G::generator();
-    let c: u32 = 9;
-    let bucket_size = 1 << c;
-    let buckets = (1..=bucket_size).map(|_| generator).collect::<Vec<G>>();
-
-    // let cpu_naive_result = cpu_naive_bpr(&buckets);
-    let cpu_pbpr_result = cpu_parallel_bpr(&buckets);
-
-    let start = Instant::now();
-    let gpu_pbpr_big_int_result = gpu_parallel_bpr_big_int(&buckets);
-    let duration = start.elapsed();
-    println!("gpu_parallel_bpr_big_int execution time: {:?}", duration);
-    assert_eq!(gpu_pbpr_big_int_result, cpu_pbpr_result);
 }
